9f1cc584103bc858ce7cdee69e6590bd95f5dd74
Tsubasa Hizono <tsubasa.robotics@gmail.com>
2024-11-20 08:30:44 +0900

    プロジェクト追加

---
+++ ShogiAIPractice.Main/Program.cs
@@ -0,0 +1,27 @@
+﻿using MyShogi.Model.Shogi.Core;
+using MyShogi.Model.Shogi.Converter;
+
+Initializer.Init();
+bool onGame = true;
+Position position = new Position();
+position.InitBoard();
+
+while (onGame)
+{
+    var command = Console.ReadLine();
+    
+    Console.Clear();
+    Console.WriteLine(position.Pretty());
+    
+    switch (command)
+    {
+        case "end":
+            onGame = false;
+            break;
+        default:
+            Console.WriteLine("not supported command");
+            break;
+    }
+}
+
+Console.WriteLine("GameEnd");
