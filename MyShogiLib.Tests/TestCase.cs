b90ea2a8849645a42d4897b291917b0b99ead979
Tsubasa Hizono <tsubasa.robotics@gmail.com>
2024-12-09 08:38:11 +0900

    テストケース追加

--- MyShogiLib.Tests/TestCase.cs
+++ MyShogiLib.Tests/TestCase.cs
@@ -72,5 +72,35 @@ namespace MyShogiLib.Tests
 			// 正しく復元できているか？
 			Assert.Equal(position2.ToSfen(), position3.ToSfen());
 		}
+		
+		/// <summary>
+		/// 局面「lr3g1n+B/3sgbk1l/p2p1p1s1/2P1p1pp1/2pS4p/4PPP2/P+p3S1P1/3RG1GK1/LN5NL w N3p 1」のpack、unpackテスト
+		/// </summary>
+		[Fact]
+		public void TestPackedSfen2()
+		{
+			// 盤面の初期化
+			Position position = new Position();
+			position.InitBoard();
+			
+			// 平手盤面になっているかどうか？
+			Assert.Equal(Sfens.HIRATE, position.ToSfen());
+			
+			var packedSfen = new int[]
+			{
+				141, 143, 241, 23, 175, 245, 11, 231, 251, 105, 76, 82, 58, 17, 82, 38, 242, 128, 132, 136, 32, 26, 135, 128, 159, 199, 99, 88, 192, 50, 20, 146, 36, 73, 146, 36
+			};
+			
+			var testSfen = "lr3g1n+B/3sgbk1l/p2p1p1s1/2P1p1pp1/2pS4p/4PPP2/P+p3S1P1/3RG1GK1/LN5NL w N3p 1";
+			var testPackedSfenStr = string.Join(" ", packedSfen);
+			
+			// 盤面をunpackしてみる
+			
+			var sfen = SfenConverter.Unpack(packedSfen);
+			position.SetSfen(sfen);
+			
+			// 正しく復元できているか？
+			Assert.Equal(testSfen, position.ToSfen());
+		}
 	}
 }
