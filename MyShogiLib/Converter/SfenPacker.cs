b749a555b0e5f98912591608df9a79e881a76e86
Tsubasa Hizono <tsubasa.robotics@gmail.com>
2024-12-12 07:29:36 +0900

    SfenPackerへメソッド追加

--- MyShogiLib/Converter/SfenPacker.cs
+++ MyShogiLib/Converter/SfenPacker.cs
@@ -80,7 +80,67 @@ public class BitStream
 	}
 }
 
+public struct HuffmanedPiece
+{
+	public int Code; // どうコード化されるか
+	public int Bits; // 何bit専有するのか
+
+	public HuffmanedPiece(int code, int bits)
+	{
+		Code = code;
+		Bits = bits;
+	}
+}
+
+public static class HuffmanTables
+{
+	public static readonly HuffmanedPiece[] HuffmanTable = new HuffmanedPiece[]
+	{
+		new HuffmanedPiece(0x00, 1), // NO_PIECE
+		new HuffmanedPiece(0x01, 2), // PAWN
+		new HuffmanedPiece(0x03, 4), // LANCE
+		new HuffmanedPiece(0x0b, 4), // KNIGHT
+		new HuffmanedPiece(0x07, 4), // SILVER
+		new HuffmanedPiece(0x1f, 6), // BISHOP
+		new HuffmanedPiece(0x3f, 6), // ROOK
+		new HuffmanedPiece(0x0f, 5), // GOLD
+	};
+
+	public static readonly HuffmanedPiece[] HuffmanTablePieceBox = new HuffmanedPiece[]
+	{
+		new HuffmanedPiece(0x00, 1), // not use
+		new HuffmanedPiece(0x02, 2), // PAWN
+		new HuffmanedPiece(0x09, 4), // LANCE
+		new HuffmanedPiece(0x0d, 4), // KNIGHT
+		new HuffmanedPiece(0x0b, 4), // SILVER
+		new HuffmanedPiece(0x2f, 6), // BISHOP
+		new HuffmanedPiece(0x3f, 6), // ROOK
+		new HuffmanedPiece(0x1b, 5), // GOLD
+	};
+}
+
 public class SfenPacker
 {
+	public static byte[] data = new byte[32];
+	public static BitStream bitStream = new BitStream();
+	
+	// 盤面の駒をstreamに出力する。
+	public static void WriteBoardPieceToStream(Piece pc)
+	{
+		// 駒種
+		Piece pr = pc.RawPieceType();
+		HuffmanedPiece c = HuffmanTables.HuffmanTable[(int)pr];
+		bitStream.WriteNBit(c.Code, c.Bits);
 
+		if (pc == Piece.NO_PIECE)
+			return;
+
+		// 成りフラグ
+		// (金はこのフラグはない)
+		if (pr != Piece.GOLD)
+			bitStream.WriteOneBit((pc & Piece.PROMOTE) != 0 ? 1 : 0);
+
+		// 先後フラグ
+		bitStream.WriteOneBit((int)pc.PieceColor());
+	}
 }
